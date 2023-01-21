66f01243d2cdf976c26ff77f90d899fadf2fe08b
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-01-21 15:52:00 -0500

    minor refatoring

--- src/components/GalleryNavigation/index.js
+++ src/components/GalleryNavigation/index.js
@@ -2,27 +2,19 @@ import { NavLink } from "react-router-dom";
 import './GalleryNavigation.css';
 
 const GalleryNavigation = props => {
-
-    const galleries = Object.values(props)[0];
- 
-    const galleriesList = galleries.map(gallery => {
-        return (
-        
-            <NavLink to={`/galleries/${gallery.id}`} className='gallery-item' key={gallery.id}>{gallery.name}</NavLink>
-        
-        )
-    })
-    
     return (
         <nav>
-            <NavLink to="/" className="home-page">Home</NavLink>
-            <h1>Galleries </h1>
-            <ul className="gallery-list">
-                {galleriesList}
-            </ul>
+            <h1 className="nav-h">Galleries </h1>
+            <NavLink exact to="/" className="home-page">Home</NavLink>
+            {props.galleries.map(gallery => {
+                return (
+                    <div key={gallery.id}>
+                        <NavLink to={`/galleries/${gallery.id}`} className='gallery-item'>{gallery.name}</NavLink>
+                    </div>
+                )
+            })}
         </nav>
     )
-
-}
+};
 
 export default GalleryNavigation;
